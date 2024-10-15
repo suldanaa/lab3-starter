151f9197283924681fad147f2f7116aed4222767
Abdulkadir Afrah <sabdulkadirafrah@ieng6-203.ucsd.edu>
2024-10-15 13:22:49 -0700

    Fixed bug for calculating average

--- average.c
+++ average.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Calculate the average of all args
-    float average = sum / 10;
+    float average = sum / 2;
 
     printf("Average: %.2f\n", average);
 
